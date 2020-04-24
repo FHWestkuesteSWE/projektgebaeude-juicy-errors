294301f396d675f351982bdc1009b41f0812a299
Sören Kahl <Soeren.Kahl@haw-hamburg.de>
2020-04-24 12:01:10 +0200

    Add set functions for Actuator Class

--- Software/GebaeudeManagement/Server/Actuator.cpp
+++ Software/GebaeudeManagement/Server/Actuator.cpp
@@ -1,1 +1,11 @@
 #include "Actuator.h"
+
+void Actuator::setBooleanValue(bool val)
+{
+	_bVal = val;
+}
+
+void Actuator::setDoubleValue(double val)
+{
+	_fVal = val;
+}
--- Software/GebaeudeManagement/Server/Actuator.h
+++ Software/GebaeudeManagement/Server/Actuator.h
@@ -2,5 +2,11 @@
 
 	class Actuator
 	{
+	private:
+		// private attributes
+		bool _bVal;
+		double _fVal;
 	public:
+		void setBooleanValue(bool val);
+		void setDoubleValue(double val);
 	};
