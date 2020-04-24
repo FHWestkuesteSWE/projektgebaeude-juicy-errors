28c425ff464aa83fca121951b5ded3c089a14834
Sören Kahl <Soeren.Kahl@haw-hamburg.de>
2020-04-24 11:37:03 +0200

    Add functions to Sensor class with random return values

--- Software/GebaeudeManagement/Server/Sensor.cpp
+++ Software/GebaeudeManagement/Server/Sensor.cpp
@@ -1,1 +1,38 @@
 #include "Sensor.h"
+#include <stdlib.h>
+#include<time.h> 
+
+bool Sensor::getBooleanValue()
+{
+	srand(time(0));
+	int r = rand() % 100;
+	if (r > 80)
+		bVal = true;
+	else
+		bVal = false;
+
+	return bVal;
+}
+
+double Sensor::getDoubleValue(double fMin, double fMax, double lastValue)
+{
+	srand(time(0));
+	double dev;
+	int r = rand() % 100; // random integer between 0..100
+	double f = (double)rand() / RAND_MAX; // random value between 0..1
+
+	if(lastValue > 0.0)
+	{
+		dev = 0.1 * lastValue * f; // deviation between 0..10%
+
+		if (r > 50) 
+			fVal = lastValue + dev;
+		else
+			fVal = lastValue - dev;
+	}
+	else // no value predefined
+	{
+		fVal = fMin + f * (fMax - fMin);
+	}
+	return fVal;
+}
--- Software/GebaeudeManagement/Server/Sensor.h
+++ Software/GebaeudeManagement/Server/Sensor.h
@@ -1,5 +1,13 @@
 #pragma once
 class Sensor
 {
+private:
+	// private attributes
+	bool bVal;
+	double fVal;
+
+public:
+	bool getBooleanValue();
+	double getDoubleValue(double fMin, double fMax, double lastValue);
 };
 
