f781dd53de18a83e0e26982fb725c0b83e07985d
Fabian Huber <Fabian.Huber@haw-hamburg.de>
2020-04-25 18:34:43 +0200

    Added client functionality to compose request strings and commit them to server

--- Software/GebaeudeManagement/Client/BasicClient.cpp
+++ Software/GebaeudeManagement/Client/BasicClient.cpp
@@ -1,26 +1,31 @@
 #include "BasicClient.h"
 
 
+// constructor
 BasicClient::BasicClient(char _server[], char _port[]) {
-	server = _server;
-	port = _port;
+  server = _server;
+  port = _port;
 }
+
+// send request to server
 void BasicClient::sendRequest(const char request[], char answer[]) {
-	boost::asio::io_service io_service;
+  boost::asio::io_service io_service;
 
-	tcp::resolver resolver(io_service);
-	tcp::resolver::query query(tcp::v4(), server, port);
-	tcp::resolver::iterator iterator = resolver.resolve(query);
+  tcp::resolver resolver(io_service);
+  tcp::resolver::query query(tcp::v4(), server, port);
+  tcp::resolver::iterator iterator = resolver.resolve(query);
 
-	tcp::socket s(io_service);
-	boost::asio::connect(s, iterator);
+  tcp::socket s(io_service);
+  boost::asio::connect(s, iterator);
 
-	size_t request_length = strlen(request)+1;
-	boost::asio::write(s, boost::asio::buffer(request, request_length));
+  size_t request_length = strlen(request)+1;
+  boost::asio::write(s, boost::asio::buffer(request, request_length));
 
-	size_t reply_length = boost::asio::read(s,
-		boost::asio::buffer(answer, max_length));
+  size_t reply_length = boost::asio::read(s,
+    boost::asio::buffer(answer, max_length));
 }
+
+// destructor
 BasicClient::~BasicClient() {
 
 }
