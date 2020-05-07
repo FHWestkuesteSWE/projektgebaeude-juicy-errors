aaa558daa4453f66a233734e4da486f61fb621ec
Sören Kahl <Soeren.Kahl@haw-hamburg.de>
2020-05-07 13:47:23 +0200

    Added two TestMethods for Sensor getDouble

--- Software/GebaeudeManagement/UnitTestServer/UnitTestServer.cpp
+++ Software/GebaeudeManagement/UnitTestServer/UnitTestServer.cpp
@@ -26,5 +26,49 @@ namespace UnitTestServer
 			else
 				Assert::IsTrue(true);
 		}
+
+		TEST_METHOD(TestSensorGetDoubleValueCorrectDeviation)
+		{
+			Sensor sensor = Sensor();
+			double fMin = 0.0;
+			double fMax = 5.0;
+			double lastValue = 3.0;
+			double val = sensor.getDoubleValue(fMin, fMax, lastValue);
+
+
+			// return value in 20% range around lastValue
+			if (val >= lastValue + 0.1 * lastValue && val <= lastValue - 0.1 * lastValue)
+			{
+				Assert::IsTrue(false);
+			}
+			else
+				Assert::IsTrue(true);
+		}
+
+		TEST_METHOD(TestSensorGetDoubleValueFalseParameters)
+		{
+			Sensor sensor = Sensor();
+			double fMin = 5.0;
+			double fMax = 1.0;
+			double lastValue = 3.0;
+			double val1 = sensor.getDoubleValue(fMin, fMax, lastValue);
+			//fMin>fMax
+			Assert::AreEqual(-1.0, val1);
+
+			fMin = 1.0;
+			fMax = 2.0;
+			lastValue = 0.0;
+			double val2 = sensor.getDoubleValue(fMin, fMax, lastValue);
+			// lastValue < fMin
+			Assert::AreEqual(fMin, val2);
+
+			fMin = 1.0;
+			fMax = 2.0;
+			lastValue = 3.0;
+			double val3 = sensor.getDoubleValue(fMin, fMax, lastValue);
+			// lastValue > fMin
+			Assert::AreEqual(fMax, val3);
+		}
+
 	};
 }
