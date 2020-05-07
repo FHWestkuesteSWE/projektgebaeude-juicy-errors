18cd1a94936098daca2447e1322db2d80d3ca5da
Sören Kahl <Soeren.Kahl@haw-hamburg.de>
2020-05-07 13:13:10 +0200

    Added test project for Server

---
+++ Software/GebaeudeManagement/UnitTestServer/UnitTestServer.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestServer
+{
+	TEST_CLASS(UnitTestServer)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
