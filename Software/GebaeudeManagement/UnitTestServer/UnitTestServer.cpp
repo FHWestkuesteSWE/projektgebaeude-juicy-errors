8865e8aabf00c806e04ad6ee9c8ef0e7702fbdea
Sören Kahl <Soeren.Kahl@haw-hamburg.de>
2020-05-07 13:30:43 +0200

    Added TestMethod for Sensor getDouble in range

--- Software/GebaeudeManagement/UnitTestServer/UnitTestServer.cpp
+++ Software/GebaeudeManagement/UnitTestServer/UnitTestServer.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "Sensor.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,8 +10,23 @@ namespace UnitTestServer
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		//Tests for class Sensor:
+		TEST_METHOD(TestSensorGetDoubleValueInRange)
 		{
+			Sensor sensor = Sensor();
+			double fMin = 0.0;
+			double fMax = 5.0;
+			double lastValue = 3.0;
+			double val = sensor.getDoubleValue(fMin, fMax, lastValue);
+
+			// return value in 20% range around lastValue
+			if (val <= lastValue + 0.1 * lastValue && val >= lastValue - 0.1 * lastValue)
+			{
+				Assert::IsTrue(true);
+			}
+			else
+				Assert::IsTrue(false);;
+
 		}
 	};
 }
