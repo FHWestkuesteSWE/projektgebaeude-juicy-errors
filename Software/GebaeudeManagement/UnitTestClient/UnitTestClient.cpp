aa073f6536fb86814c07947a9b79c0743842e4a7
Fabian Huber <fahu@gmx.de>
2020-05-08 10:17:11 +0200

    + Added unit tests for client

---
+++ Software/GebaeudeManagement/UnitTestClient/UnitTestClient.cpp
@@ -0,0 +1,47 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest_Client
+{
+  TEST_CLASS( UnitTest_askForValue )
+  {
+
+  public:
+    
+    TEST_METHOD( descrIsEmpty )
+    {
+      char descr[2] = ""; 
+      roomDescriptors.clear(); // clear list
+      roomDescriptors.push_back ("kitchen1"); // fill roomDescriptors list with item
+      int roomId = 0; 
+
+      Assert::AreEqual( askForValue (descr, roomId ), -1 ); 
+    }
+
+    TEST_METHOD( descrNotZeroTerminated )
+    {
+      char descr[2]; 
+      descr[0] = 'f';
+      descr[1] = 'o';
+      descr[2] = 'o';
+      roomDescriptors.clear(); // clear list
+      roomDescriptors.push_back ("kitchen1"); // fill roomDescriptors list with item
+      int roomId = 0; 
+
+      Assert::AreEqual( askForValue (descr, roomId ), -1 ); 
+    }
+    
+    
+    TEST_METHOD( roomIdInRange )
+    {
+      char descr[10] = "kitchen1"; 
+      roomDescriptors.clear(); // clear list
+      roomDescriptors.push_back ("kitchen1"); // fill roomDescriptors list with item
+      int roomId = 1; // item does not exist
+
+      Assert::AreEqual( askForValue (descr, roomId ), -1 ); 
+    }
+  };
+}
