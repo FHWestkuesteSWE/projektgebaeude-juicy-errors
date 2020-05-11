3655910c59963f60f6df82b542bc6674a4b18f4f
FelixPasson <52278285+FelixPasson@users.noreply.github.com>
2020-05-12 01:02:13 +0200

    Added txt methods exists() and putline()
    txt_exists() checks if a given file exists and returns true or false respectively.
    
    txt_putLine() writes a single string to a given file. This function does not create a new file in case the file does not exist yet or the filename is corrupted.

--- Software/GebaeudeManagement/Server/FileHandling.cpp
+++ Software/GebaeudeManagement/Server/FileHandling.cpp
@@ -2,18 +2,18 @@
 
 
 // append string vector DATA by the contents of istream IN
-int loadCSV(const char *filename, vector<string>& data) {
+bool txt_read(const char *filename, vector<string> &data) {
 	string temp;
-	std::ifstream in;
-	in.open(filename, std::ifstream::in);
+	std::ifstream file;
+	file.open(filename, std::ifstream::in);
 
 	// return EXIT_FAILURE if config file does not exist or cant be accessed
-	if (!in.good()) {
-		return EXIT_FAILURE;
+	if (!file.good()) {
+		return false;
 	}
 
-	while (!in.eof()) {
-		getline(in, temp, '\n');                    // get next line
+	while (!file.eof()) {
+		getline(file, temp, '\n');					// get next line
 		data.push_back(temp);						// append vector
 
 		// DEBUG OUTPUT
@@ -21,18 +21,76 @@ int loadCSV(const char *filename, vector<string>& data) {
 
 		temp.clear();
 	}
-	in.close();
+	file.close();
 
-	return EXIT_SUCCESS;
+	return true;
+}
+
+
+// Truncate file with content of DATA, overwriting all previous data in the process
+bool txt_truncate(const char* filename, vector<string>& data) {
+	fstream file(filename);								// opens file if file exists, does not create new file if file doesn't exist
+
+	// Check if a config file exists
+	if (!txt_exists(filename)) {
+
+		// if file does not exist, create new file
+		file.open(filename, ios::out | ios::trunc);		// this is what creates the new file
+
+		// return EXIT_FAILURE if config file could not be created
+		if (!file.is_open()) {
+			file.clear();
+			return false;
+		}
+	}
+	file.close();
+
+	// write to existing config file
+	file.open(filename, ios::out | ios::trunc);
+	if (file.is_open()) {
+		for (auto i = data.cbegin(); i != data.cend(); i++) {
+			file << *i << '\n';
+		}
+		file.close();
+	} else {
+		file.clear();
+		return false;
+	}
+	
+	return true;
 }
 
 
 
 // append string to file
-int writeCSV(ostream& out, string data) {
+bool txt_putLine(const char* filename, string &line) {
+	fstream file(filename);								// opens file if file exists, does not create new file if file doesn't exist
 
-	data = "\n" + data;
-	out.write(data.c_str(), data.size());
+	// Check if a config file exists
+	if (!txt_exists(filename)) {
+		file.clear();
+		return false;
+	}
+	file.close();
+	
+	// write to existing config file
+	file.open(filename, ios::out | ios::app);
+	if (file.is_open()) {
+		file << line << '\n';
+		file.close();
+	}
+	else {
+		file.clear();
+		return false;
+	}
+	
+	return true;
+}
 
-	return EXIT_SUCCESS;
+
+// check if a file exists, returns TRUE if exists and FALSE if does not exist
+bool txt_exists(const char* filename) {
+	ifstream file(filename);
+
+	return file.good();
 }
